0f950ee597a18c7b6fe5a896467c8416e55aad72
abhi_21094 <abhisharma.sharma7@gmail.com>
2019-07-29 12:46:03 +0530

    check hook

--- contracts/ZUCToken.sol
+++ contracts/ZUCToken.sol
@@ -1,5 +1,5 @@
 /**
- *Submitted for verification at Etherscan.io on 2019-07-28
+ *Submitted for verification at Etherscan.io on 2019-07-29
 */
 
 // File: openzeppelin-solidity/contracts/token/ERC20/IERC20.sol
