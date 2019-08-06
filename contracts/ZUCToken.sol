21f4674fabcbb82e8f6dfc48eaef48438e812974
abhi_21094 <abhisharma.sharma7@gmail.com>
2019-08-06 18:40:42 +0530

    check app

--- contracts/ZUCToken.sol
+++ contracts/ZUCToken.sol
@@ -1,5 +1,5 @@
 /**
- *Submitted for verification at Etherscan.io on 2019-07-31
+ *Submitted for verification at Etherscan.io on 2019-08-01
 */
 
 // File: openzeppelin-solidity/contracts/token/ERC20/IERC20.sol
